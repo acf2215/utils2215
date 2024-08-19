83b2ac135996451a44ccbbe74af5d7b449d480b2
acf2215 <acf2215bc@gmail.com>
2024-08-19 19:00:20 +0900

    add ContentNegotiation

--- AutoComplete/HttpHeaders/index.ts
+++ AutoComplete/HttpHeaders/index.ts
@@ -2,10 +2,12 @@ import { Authentication } from "./Authentication";
 import { Caching } from "./Caching";
 import { Conditionals } from "./Conditionals";
 import { ConnectionManagement } from "./ConnectionManagement";
+import { ContentNegotiation } from "./ContentNegotiation";
 
 export type HttpHeaders = 
   | Authentication 
   | Caching 
   | Conditionals
   | ConnectionManagement
+  | ContentNegotiation
   | (string & {}); 
