20d5d44636abffb54c7eb56e34ec397fc052fc8a
acf2215 <acf2215bc@gmail.com>
2024-08-19 18:56:28 +0900

    add ConnectionManagement

---
+++ AutoComplete/HttpHeaders/ConnectionManagement/index.ts
@@ -0,0 +1,3 @@
+export type ConnectionManagement =
+  | 'Connection'
+  | 'Keep-Alive';
--- AutoComplete/HttpHeaders/index.ts
+++ AutoComplete/HttpHeaders/index.ts
@@ -1,9 +1,11 @@
 import { Authentication } from "./Authentication";
 import { Caching } from "./Caching";
 import { Conditionals } from "./Conditionals";
+import { ConnectionManagement } from "./ConnectionManagement";
 
 export type HttpHeaders = 
   | Authentication 
   | Caching 
   | Conditionals
+  | ConnectionManagement
   | (string & {}); 
